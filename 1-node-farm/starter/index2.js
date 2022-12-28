34d244826b2e9b1979afb13bed7f7e1ea936060d
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-12-28 20:38:51 +0330

    add content type and header to response

--- 1-node-farm/starter/index2.js
+++ 1-node-farm/starter/index2.js
@@ -1,8 +1,21 @@
 //! Server
 const http = require('node:http')
+const url = require('url')
+
 const server = http.createServer((req, res) => {
-  console.log(req)
-  res.end('Welcome')
+  const pathName = req.url
+
+  if (pathName === '/' || pathName === '/overview') {
+    res.end('This is the overview')
+  } else if (pathName === '/product') {
+    res.end('This is the product')
+  } else {
+    res.writeHead(404, {
+      'Content-type': 'text/html',
+      'my-own-header': 'Not found idiot',
+    })
+    res.end('<h1>Not found 404!</h1>')
+  }
 })
 
 server.listen(8000, '127.0.0.1', () => {
