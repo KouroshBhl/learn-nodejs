d5038492eae18f55e617dc3f9f8539a5852652b2
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-12-28 20:59:00 +0330

    read file sync and send it as object to user

--- 1-node-farm/starter/index2.js
+++ 1-node-farm/starter/index2.js
@@ -1,6 +1,9 @@
 //! Server
+const fs = require('fs')
 const http = require('node:http')
 const url = require('url')
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
 
 const server = http.createServer((req, res) => {
   const pathName = req.url
@@ -9,6 +12,9 @@ const server = http.createServer((req, res) => {
     res.end('This is the overview')
   } else if (pathName === '/product') {
     res.end('This is the product')
+  } else if (pathName === '/api') {
+    res.writeHead(200, { 'content-type': 'application/json' })
+    res.end(data)
   } else {
     res.writeHead(404, {
       'Content-type': 'text/html',
