fe11c63294b19de264f9e862faaa7d2b32a4e3ec
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-12-29 10:27:57 +0330

    add some functions to make ui

--- 1-node-farm/starter/index2.js
+++ 1-node-farm/starter/index2.js
@@ -3,13 +3,49 @@ const fs = require('fs')
 const http = require('node:http')
 const url = require('url')
 
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName)
+  output = output.replace(/{%IMAGE%}/g, product.image)
+  output = output.replace(/{%PRICE%}/g, product.price)
+  output = output.replace(/{%FROM%}/g, product.from)
+  output = output.replace(/{%NUT%}/g, product.nutrients)
+  output = output.replace(/{%QUANTITY%}/g, product.quantity)
+  output = output.replace(/{%DESCRIPTION%}/g, product.description)
+  output = output.replace(/{%ID%}/g, product.id)
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic')
+  return output
+}
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8')
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  'utf-8'
+)
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  'utf-8'
+)
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  'utf-8'
+)
+const dataObj = JSON.parse(data)
 
 const server = http.createServer((req, res) => {
   const pathName = req.url
 
   if (pathName === '/' || pathName === '/overview') {
-    res.end('This is the overview')
+    res.writeHead(200, { 'content-type': 'text/html' })
+
+    const cardsHtml = dataObj
+      .map((card) => replaceTemplate(tempCard, card))
+      .join()
+
+    const output = tempOverview.replace('{%PRODUCT_CARD%}', cardsHtml)
+    console.log(cardsHtml)
+
+    res.end(output)
   } else if (pathName === '/product') {
     res.end('This is the product')
   } else if (pathName === '/api') {
