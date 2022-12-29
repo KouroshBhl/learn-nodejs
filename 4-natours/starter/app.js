6316c1db83ccd267eb9c1173accbeff43f717ded
Kourosh Baharloo <kourosh9bhl@gmail.com>
2022-12-29 19:56:59 +0330

    Refactor some codes

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -8,15 +8,25 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 )
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     length: tours.length,
     data: { tours },
   })
-})
+}
 
-app.post('/api/v1/tours', (req, res) => {
+const getTour = (req, res) => {
+  const tour = tours.find((tour) => +req.params.id === tour.id)
+  if (!tour)
+    return res
+      .status(404)
+      .json({ status: 'fail', message: 'we could not find any data' })
+
+  return res.status(200).json({ status: 'success', data: { tour } })
+}
+
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1
   const newTour = Object.assign({ id: newId }, req.body)
 
@@ -33,7 +43,35 @@ app.post('/api/v1/tours', (req, res) => {
       })
     }
   )
-})
+}
+
+const updateTour = (req, res) => {
+  const tour = tours.find((tour) => +req.params.id === tour.id)
+
+  if (!tour)
+    return res
+      .status(404)
+      .json({ status: 'fail', message: 'we could not find any data' })
+
+  return res.status(200).json({ status: 'success', data: '<Updated Tour>' })
+}
+
+const deleteTour = (req, res) => {
+  const tour = tours.find((tour) => +req.params.id === tour.id)
+
+  if (!tour) return res.status(204).json({ status: 'fail', message: null })
+
+  return res.status(200).json({ status: 'success', data: '<Deleted Tour>' })
+}
+
+// app.get('/api/v1/tours', getAllTours)
+// app.get('/api/v1/tours/:id', getTour)
+// app.post('/api/v1/tours', createTour)
+// app.patch('/api/v1/tours/:id', updateTour)
+// app.delete('/api/v1/tours/:id', deleteTour)
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour)
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour)
 
 const port = 3000
 app.listen(port, () => {
